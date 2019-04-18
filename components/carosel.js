9d126b75c460e4c49a86f67336ac6948fafebdaa
moddamatt <echomatt95@gmail.com>
2019-04-17 20:17:33 -0500

    Set up links and headshots

---
+++ components/carosel.js
@@ -0,0 +1,60 @@
+// setsup the carosel
+$(document).ready(function() {
+  $(".carosel").slick({
+    accessibilty: true,
+    arrows: false,
+    autoplay: true
+
+  });
+});
+
+// Select the teammember class so that we may effect it
+let member = document.querySelectorAll('.teamMember');
+
+// An array of socail data to be used for clickable buttons
+let socialData = {
+  amanda: [],
+  matthew: ['https://github.com/moddamatt', 'https://www.linkedin.com/in/mattmorganlewis/'],
+  javontay: ['https://github.com/javontaymcelroy','https://www.linkedin.com/in/javontay-mcelroy-663b81bb/'],
+  alex: [],
+  will: ['https://github.com/brellin', 'https://www.linkedin.com/in/brellin/'],
+  curtis: ['https://github.com/Chubbard022', 'https://www.linkedin.com/in/curtis-hubbard-945764158/']
+};
+
+class People {
+  constructor(person) {
+      this.person = person;
+      this.data = this.person.dataset.person;
+      this.person.style.background = `linear-gradient( rgba(0, 0, 0, 0.25), rgba(50, 50, 50, 0.45)), url('assets/headshots/${this.data}.jpg') scroll no-repeat center / contain`;
+  }
+}
+
+class Plinks extends People {
+  constructor(person) {
+      super(person);
+      this.person = person;
+      this.name = this.person.querySelector('h3').innerText;
+      this.position = this.person.querySelector('h4').innerText;
+      this.id = this.person.id;
+      // GitHub
+      this.gh = document.createElement('a');
+      this.gh.href = `${socialData[this.id][0]}`;
+      let git = document.createElement('img');
+      git.src = 'assets/gh.png';
+      git.classList.add('git');
+      this.gh.appendChild(git);
+      // LinkedIn
+      this.in = document.createElement('a');
+      this.in.href = `${socialData[this.id][1]}`;
+      let linkedIn = document.createElement('img');
+      linkedIn.src = 'assets/in.png';
+      linkedIn.classList.add('in');
+      this.in.appendChild(linkedIn);
+      // Add to .person
+      this.person.prepend(this.in);
+      this.person.prepend(this.gh);
+  }
+}
+
+member.forEach(per => new People(per));
+member.forEach(per => new Plinks(per));
